67a20b122cf183831c28976fd0130d85b75be03e
dsevostyanov <dsevostyanov@phoenixit.ru>
2021-01-26 22:23:39 +0300

    task 1

---
+++ Lesson1/Task1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1
+{
+    class Task1
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Write("Имя: ");
+            string firstName = Console.ReadLine();
+
+            Console.Write("Фамилия: ");
+            string lastName = Console.ReadLine();
+
+            Console.Write("Возраст: ");
+            string age = Console.ReadLine();
+
+            Console.Write("Рост: ");
+            string height = Console.ReadLine();
+
+            Console.Write("Вес: ");
+            string weight = Console.ReadLine();
+
+
+            string a = firstName + " " + lastName + " " + age + " " + height + " " + weight;
+            string b = String.Format("{0} {1} {2} {3} {4}", firstName, lastName, age, height, weight);
+            string c = $"{firstName} {lastName} {age} {height} {weight}";
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+
+            Console.ReadKey();
+        }
+    }
+}
