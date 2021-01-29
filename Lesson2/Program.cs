605e5df7ab0cb15380f863d82535346f0806f71f
dsevostyanov <dsevostyanov@phoenixit.ru>
2021-01-29 21:52:00 +0300

    task 3

--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -10,7 +10,7 @@ namespace Lesson2
     {
         static void Main(string[] args)
         {
-            new Task2().Perform();
+            new Task3().Perform();
         }
     }
 }
---
+++ Lesson2/Task3.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2
+{
+    class Task3
+    {
+        public void Perform()
+        {
+            long sum = 0;
+            bool isNumber;
+            int number;
+
+            do
+            {
+                Console.Write("Введите число: ");
+                string input = Console.ReadLine();
+                isNumber = int.TryParse(input, out number);
+
+                if (isNumber && IsPositiveOdd(number))
+                    sum += number;
+            }
+            while (isNumber && number != 0);
+
+            Console.WriteLine($"Результат: {sum}");
+
+            Console.ReadKey();
+        }
+
+        private bool IsPositiveOdd(int n)
+        {
+            return n > 0 && n % 2 == 1;
+        }
+    }
+}
