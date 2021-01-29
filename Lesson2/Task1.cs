09bab9a827a00f85171504af5f95a97c33b2ebc2
dsevostyanov <dsevostyanov@phoenixit.ru>
2021-01-29 21:26:52 +0300

    task 1

---
+++ Lesson2/Task1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2
+{
+    class Task1
+    {
+        public void perform()
+        {
+            Random random = new Random();
+            int a = random.Next(100);
+            int b = random.Next(100);
+            int c = random.Next(100);
+
+            int min = Min(a, b, c);
+
+            Console.WriteLine($"Числа: {a} {b} {c}");
+            Console.WriteLine($"Минимальное: {min}");
+
+            Console.ReadKey();
+        }
+
+        public int Min(int a, int b, int c)
+        {
+            int min = a < b ? a : b;
+            min = min < c ? min : c;
+
+            return min;
+        }
+    }
+}
