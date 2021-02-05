af4666197fdc510d5e3500e5b275f17020f7d416
dsevostyanov <dsevostyanov@phoenixit.ru>
2021-02-05 21:45:40 +0300

    task1

---
+++ Lesson4/Program.cs
@@ -0,0 +1,17 @@
+﻿using Lesson4.task1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Task1().Perform();
+        }
+    }
+}
---
+++ Lesson4/task1/Task1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4.task1
+{
+    class Task1
+    {
+        public void Perform()
+        {
+            int[] array = { 6, 2, 9, -3, 6 };
+
+            int count = FindCountrPairs(array);
+
+            Console.WriteLine($"Количество пар: {count}");
+            Console.ReadKey();
+        }
+
+        private int FindCountrPairs(int[] array)
+        {
+            int count = 0;
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] % 3 == 0 || array[i + 1] % 3 == 0)
+                    count++;
+            }
+
+            return count;
+        }
+    }
+}
