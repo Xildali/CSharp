b2bdfda132a9f3909fe37e483b360bb70a25a6b8
dsevostyanov <dsevostyanov@phoenixit.ru>
2021-02-11 23:42:18 +0300

    task1

---
+++ Lesson6/task1/Task1.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * Севостьянов Денис
+ * 
+ * Изменить программу вывода функции так, чтобы можно было передавать функции типа double (double,double). 
+ * Продемонстрировать работу на функции с функцией a*x^2 и функцией a*sin(x).
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson6
+{
+    public delegate double Function(double a, double x);
+
+    class Task1
+    {
+        public void Perform()
+        {
+            Console.WriteLine("Таблица функции a*x^2:");
+            Table(SquareFunc, 4, -2, 2);
+
+            Console.WriteLine("Таблица функции a*sin(x):");
+            Table(SinFunc, 4, -2, 2);
+
+            Console.ReadKey();
+        }
+
+        private void Table(Function func, double a, double x, double b)
+        {
+            Console.WriteLine("----- X ----- Y -----");
+
+            while (x <= b)
+            {
+                Console.WriteLine("| {0,8:0.000} | {1,8:0.000} |", x, func(a, x));
+                x++;
+            }
+
+            Console.WriteLine("---------------------");
+        }
+
+        public static double SquareFunc(double a, double x)
+        {
+            return a * x * x;
+        }
+
+        public static double SinFunc(double a, double x)
+        {
+            return a * Math.Sin(x);
+        }
+    }
+}
